55d0283dc1598830efc9cb078f05da5034eabd82
ryanjoneil <ryanjoneil@10f34b66-a92c-0410-b755-39e9f4ab4909>
2007-04-11 00:59:41 +0000

    Moving regression demo back to selection ranges.

--- trunk/pygep/src/demo/regression.py
+++ trunk/pygep/src/demo/regression.py
@@ -5,17 +5,14 @@ import math, random
 
 # The function we are trying to find
 def target(x):
-    #return 3 * (x ** 2) + (2 * x)
-
-    # 3a^2 + 2b^3 + 1
-    return 3 * (x.a ** 2) + 2 * (x.b ** 3) + 1
+    # 3x^2 + 2x + 1
+    return 3*(x.a**2) + (2*x.a) + 1
 
 
 # Generate a random sample to test against
 class Data(object):
-    def __init__(self, a, b):
+    def __init__(self, a):
         self.a = float(a)
-        self.b = float(b)
 
 
 # The functions we use in our chromosome
@@ -40,33 +37,22 @@ def divide(x, y):
 # The chromsomes: fitness is accuracy over the sample
 class Regression(Chromosome):
     functions = multiply, add, subtract, divide
-    terminals = 'a', 'b', 1, 2
+    terminals = 'a', 1, 2
 
+    selection_range = 100.0
     range_low, range_high = -10.0, 10.0
     range_size = range_high - range_low
-    sample = [Data(range_low + (random.random() * range_size),
-                   range_low + (random.random() * range_size)) for _ in xrange(10)]
-
+    sample = [Data(range_low + (random.random() * range_size)) for _ in xrange(10)]
+    
     def _fitness(self):
-        total = 0
-        for x in self.sample:
-            try:
-                diff = 1 - (self.evaluate(x) / float(target(x)))
-                if diff < 0.001:
-                    total += 20
-                if diff < 0.01:
-                    total += 10
-                elif diff < 0.05:
-                    total += 5
-                elif diff < 0.10:
-                    total += 1
-            except: # Let nature sort out semantic errors
-                pass
-
-        return total
+        try:
+            return int(max(sum(self.selection_range - abs(self.evaluate(x)-target(x))
+                               for x in self.sample), 0.0))
+        except:
+            return 0
 
     def _solved(self):
-        return self.fitness >= (20 * len(self.sample))
+        return self.fitness >= (self.selection_range * len(self.sample))
 
 
 if __name__ == '__main__':
@@ -74,7 +60,7 @@ if __name__ == '__main__':
     p = Population(Regression, 20, 6, 3, lambda *args: sum(args))
     print p
 
-    for _ in xrange(1000):
+    for _ in xrange(50):
         if p.best.solved:
             break
         p.cycle()
