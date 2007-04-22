79596c284a0241437f22cf8852ae2ab17e989bf8
ryanjoneil <ryanjoneil@10f34b66-a92c-0410-b755-39e9f4ab4909>
2007-04-22 04:18:39 +0000

    Tagging 0.1.2 beta release.  Changes: 	- Issue #4: defaulting to random selection when fitness mean = 0 	- Adding metadata to python package

---
+++ tags/pygep-0.1.2-beta/setup.py
@@ -0,0 +1,30 @@
+from setuptools import setup, find_packages
+
+
+setup(
+    name         = 'PyGEP',
+    version      = '0.1.2',
+    license      = 'GPL',
+    description  = 'Gene Expression Programming for Python',
+    author       = "Ryan J. O'Neil",
+    author_email = 'ryanjoneil@gmail.com',
+    url          = 'http://code.google.com/p/pygep',
+    download_url = 'http://code.google.com/p/pygep/downloads/list',
+
+    install_requires = ['python>=2.5'],
+
+    package_dir = {'': 'src'},
+    packages    = find_packages('src', exclude=['tests', 'tests.*']),
+    zip_safe    = True,
+    test_suite  = 'tests',
+
+    keywords    = 'gene expression programming genetic evolutionary',
+    classifiers = [
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Science/Research',
+        'License :: OSI Approved :: GNU General Public License (GPL)',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Topic :: Scientific/Engineering :: Artificial Intelligence'
+    ]
+)
--- tags/pygep-0.1.2-beta/src/demo/regression.py
+++ tags/pygep-0.1.2-beta/src/demo/regression.py
@@ -6,8 +6,9 @@ import random
 
 
 # Data points to test against and target function
-SAMPLE = []
 class DataPoint(object):
+    SAMPLE = []
+    SAMPLE_SIZE = 10
     RANGE_LOW, RANGE_HIGH = -10.0, 10.0
     RANGE_SIZE = RANGE_HIGH - RANGE_LOW
 
@@ -15,41 +16,44 @@ class DataPoint(object):
         self.x = float(x)
 
         # The function we are trying to find
-        # f(x) = 3x^2 + 2x + 1
-        self.y = 3*(x**2) + (2*x) + 1
+        # f(x) = 4*(x**4) + 3x^2 + 2x + 1
+        self.y = 4*(x**3) + 3*(x**2) + (2*x) + 1
 
+    @staticmethod
+    def populate():
+        # Creates a random sample of data points
+        DataPoint.SAMPLE = []
+        for _ in xrange(DataPoint.SAMPLE_SIZE):
+            x = DataPoint.RANGE_LOW + (random.random() * DataPoint.RANGE_SIZE)
+            DataPoint.SAMPLE.append(DataPoint(x))
 
 
 # The chromsomes: fitness is accuracy over the sample
-SELECTION_RANGE = 100.0
 class Regression(Chromosome):
+    SELECTION_RANGE = 1000.0
     functions = multiply, add, subtract, divide
     terminals = 'x', 1, 2
 
     def _fitness(self):
         try:
-            total = sum(SELECTION_RANGE - abs(self.evaluate(x)-x.y) 
-                        for x in SAMPLE)
+            total = sum(self.SELECTION_RANGE - abs(self.evaluate(x)-x.y) 
+                        for x in DataPoint.SAMPLE)
             return int(max(total, 0.0))
         except:
             return 0
 
     def _solved(self):
-        return self.fitness >= (SELECTION_RANGE * NUM)
+        return self.fitness >= (self.SELECTION_RANGE * len(DataPoint.SAMPLE))
 
 
 if __name__ == '__main__':
-    # Create a random sample of data points
-    NUM = 10
-    for _ in xrange(NUM):
-        x = DataPoint.RANGE_LOW + (random.random() * DataPoint.RANGE_SIZE)
-        SAMPLE.append(DataPoint(x))
+    DataPoint.populate()
 
     # Search for a solution
-    p = Population(Regression, 20, 6, 3, sum_linker)
+    p = Population(Regression, 20, 8, 4, sum_linker)
     print p
 
-    for _ in xrange(50):
+    for _ in xrange(100):
         if p.best.solved:
             break
         p.cycle()
